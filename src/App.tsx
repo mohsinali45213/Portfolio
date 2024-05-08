4e5aa320a832a5b4ada724fa96b15c2f7fb6168e
mohsinali45213 <mohsinali45213.com>
2024-05-08 12:06:03 +0530

    Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
 import Header from './Components/Header'
 import Home from './Page/Home'
 import About from './Page/About'
 import Projects from './Page/Projects'
-import "./App.css"
 import Skills from './Page/Skills'
+import "./App.css"
 
 const App = () => { 
   return (
     <div className='container'>
       <Header/>
       <Home/>
-      {/* <About/> */}
       <Skills/>
-      {/* <Projects/> */}
+      <About/>
+      <Projects/>
     </div>
   )
 }
